1b5ce6d23640e0773551b2afa10d881fb0b302fc
Inv <artem@tarabakin.ru>
2023-09-13 18:41:49 +0300

    task 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int speedOfLight;
-        byte time;
-        short daysOfTheYear;
-        long distanceToNeptune;
-        float gradePointAverage;
-        double salary;
-        System.out.println("Значение переменной speedOfLight с типом int равно от -2147483648 до 2147483647.");
-        System.out.println("Значение переменной time с типом byte равно от -128 до 127.");
-        System.out.println("Значение переменной distanceToNeptune с типом long равно от –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807.");
-        System.out.println("Значение переменной gradePointAverage с типом float равно от -3.4E+38 до 3.4E+38.");
-        System.out.println("Значение переменной salary с типом double равно от -1.7E+308 до 1.7E+308.");
+        int speedOfLight = 299792458;
+        byte time = 12;
+        short daysOfTheYear = 365;
+        long distanceToNeptune = 340000000000000L;
+        float gradePointAverage = 3.8f;
+        double salary = 20.3;
+        System.out.println("Значение переменной speedOfLight с типом int равно 299792458.");
+        System.out.println("Значение переменной time с типом byte равно 12.");
+        System.out.println("Значение переменной daysOfTheYear с типом double равно 365.");
+        System.out.println("Значение переменной distanceToNeptune с типом long равно 340000000000000L.");
+        System.out.println("Значение переменной gradePointAverage с типом float равно 3.8f.");
+        System.out.println("Значение переменной salary с типом double равно 20.3.");
 
 
 
