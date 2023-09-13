b7bb50677b90a65c2fca653ffe1e15cb6f930455
Inv <artem@tarabakin.ru>
2023-09-13 18:24:23 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int speedOfLight;
+        byte time;
+        short daysOfTheYear;
+        long distanceToNeptune;
+        float gradePointAverage;
+        double salary;
+        System.out.println("Значение переменной speedOfLight с типом int равно от -2147483648 до 2147483647.");
+        System.out.println("Значение переменной time с типом byte равно от -128 до 127.");
+        System.out.println("Значение переменной distanceToNeptune с типом long равно от –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807.");
+        System.out.println("Значение переменной gradePointAverage с типом float равно от -3.4E+38 до 3.4E+38.");
+        System.out.println("Значение переменной salary с типом double равно от -1.7E+308 до 1.7E+308.");
+
+
+
+    }
 }
