cf3fe470bdbdbae002466d494383968f54091582
Inv <artem@tarabakin.ru>
2023-09-13 20:04:06 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,30 @@ public class Main {
         long distanceToNeptune = 340000000000000L;
         float gradePointAverage = 3.8f;
         double salary = 20.3;
-        System.out.println("Значение переменной" +  speedOfLight + " с типом int равно 299792458.");
-        System.out.println("Значение переменной" +  time + " с типом byte равно 12.");
-        System.out.println("Значение переменной" +  daysOfTheYear + "с типом double равно 365.");
-        System.out.println("Значение переменной" +  distanceToNeptune + "с типом long равно 340000000000000L.");
-        System.out.println("Значение переменной" +  gradePointAverage + "с типом float равно 3.8f.");
-        System.out.println("Значение переменной" +  salary + "с типом double равно 20.3.");
+        System.out.println("Значение переменной " +  speedOfLight + " с типом int равно 299792458.");
+        System.out.println("Значение переменной " +  time + " с типом byte равно 12.");
+        System.out.println("Значение переменной " +  daysOfTheYear + "с типом double равно 365.");
+        System.out.println("Значение переменной " +  distanceToNeptune + " с типом long равно 340000000000000L.");
+        System.out.println("Значение переменной " +  gradePointAverage + "с типом float равно 3.8f.");
+        System.out.println("Значение переменной " +  salary + "с типом double равно 20.3.");
+
+        speedOfLight = 27897;
+        speedOfLight = 569;
+        time = 67;
+        daysOfTheYear = -159;
+        distanceToNeptune = 987678965549L;
+        gradePointAverage = 2.786f;
+        salary = 27.12;
+
+
+
+
+
+
+
+
+
+
 
 
 
